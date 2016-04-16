66a2a2f45ce59061b8d5482854f15caed1400f3d
Hannah Ward <Hannah.ward9001@gmail.com>
2016-04-16 03:45:55 +0100

    Added monster creation screen - added tmp parts

---
+++ js/monsterparts.js
@@ -0,0 +1,30 @@
+var monster_data = {
+            eyes: [
+                    {
+                      name: "Eye test",
+                      img: "eyes/Eye_test.png",
+                      crazed: 10,
+                      slimy: 0,
+                    },
+                    { name: "Blank Eyes",
+                      img: "eyes/Eye_Blank.png",
+                      crazed: 10,
+                      slimy: 0,
+                    },
+                  ],
+    
+            mouths: [
+                      {
+                        name: "Mouth Test",
+                        img: "mouths/Mouth_Test.png",
+                        crazed: 5,
+                        slimy: 3,
+                      },
+                      { name: "Spinny Mouth",
+                        img: "mouths/Mouth_New_Test.png",
+                        crazed: 100,
+                        slimy: 0,
+                      },
+                    ],
+          };
+        
--- js/states/monstercreator.js
+++ js/states/monstercreator.js
@@ -4,19 +4,51 @@ MonsterCreator.prototype = {
   //This screen will allow the user to choose body parts
   //In order to scare the child the most 
   init: function() {
-
+    
   },
 
   preload: function() {
+    game.load.image("creator_bg", "res/img/bg/Creator.png");
+    game.load.image("monster_base", "res/img/monsterparts/Monster_Base.png");
+    for (i in monster_data) {
+      console.log("Loading sprites for " + i); 
+      i = monster_data[i];
+      for (var j = 0; j < i.length; j++) {
+        obj = i[j];
+        console.log("Loading sprite " + obj["name"]);
+        game.load.image(obj["name"], "res/img/monsterparts/"+obj["img"]);
+      }                      
+    }
   },
 
   create: function() {
     console.log("Entering monster creator...");
-
+    game.add.sprite(0,0,"creator_bg");
+    game.add.sprite(300, 200, "monster_base");
+    this.parts = game.add.group();
+    var x = 200;
+    var y = 200;
+    for (i in monster_data) {
+      console.log("Adding " + i + " to canvas");
+      i = monster_data[i];
+      for (var j = 0; j < i.length; j++) {
+        obj = i[j];
+        console.log("Adding to canvas: "+j["name"]);
+        this.createpart(x,y,obj["name"], this.parts);
+        y += 50;
+      }
+    }
   },
 
+  createpart: function(x, y, sprite, grp) {
+    p = grp.create(x,y,sprite);
+    p.inputEnabled = true;
+    p.input.enableDrag(true);
+  },
+
+
   update: function() {
-  
+    game.debug.text("Monster values: ", 0, 50) 
   },
 
 
