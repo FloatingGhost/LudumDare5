b0c97af4eda363f630c117bdfaeb72b9ec2df6d3
Hannah Ward <Hannah.ward9001@gmail.com>
2016-04-16 12:32:22 +0100

    Added comfy candle

--- js/states/intelscreen.js
+++ js/states/intelscreen.js
@@ -5,15 +5,25 @@ IntelScreen.prototype = {
   },
 
   preload: function() {
-    game.load.image("scare_button", "res/img/buttons/BeginScaring.png");
+    game.load.image("scare_button", "res/img/buttons/intelread.png");
+    game.load.spritesheet("desk", "res/img/desk.png", 640, 480);
+    game.load.image("folder", "res/img/folder.png");
   },
 
   create: function() {
+    var memes = game.add.sprite(0,0,"desk");
+    memes.animations.add("flicker");
+    memes.animations.play("flicker", 10, true);
     game.child = this.createInfoText();
-    this.infoText = this.format(game.child);
+    var x = 50;
+    var y = 150;
+    for (i in game.child["evidence"]) {
+      game.add.sprite(x,y,"folder");
+      x += 250;
+    } 
     var style = { font: "16px Arial", fill: "#ff0044", align: "center" }; 
-    var text = game.add.text(100, 50,this.infoText,style); 
-    game.add.button(game.world.centerX-50, 200, "scare_button", this.scare, this, 2, 1, 0);
+    //var text = game.add.text(100, 50,this.infoText,style); 
+    game.add.button(game.world.centerX-50, 430, "scare_button", this.scare, this, 2, 1, 0);
   },
 
   scare: function() {
