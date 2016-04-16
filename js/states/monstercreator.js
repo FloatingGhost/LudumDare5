03de1c46324c607b435a42e0ac7c605b161f0993
Hannah Ward <Hannah.ward9001@gmail.com>
2016-04-16 04:35:02 +0100

    Added stats

--- js/states/monstercreator.js
+++ js/states/monstercreator.js
@@ -20,12 +20,15 @@ MonsterCreator.prototype = {
       }                      
     }
   },
-
-  create: function() {
+    
+  setupDisplay: function() {
+    //Setup visuals
     console.log("Entering monster creator...");
     game.add.sprite(0,0,"creator_bg");
-    game.add.sprite(300, 200, "monster_base");
+    this.base = game.add.sprite(300, 200, "monster_base");
+    game.physics.arcade.enable(this.base)
     this.parts = game.add.group();
+    this.lastDragged = null;
     var x = 200;
     var y = 200;
     for (i in monster_data) {
@@ -34,21 +37,75 @@ MonsterCreator.prototype = {
       for (var j = 0; j < i.length; j++) {
         obj = i[j];
         console.log("Adding to canvas: "+j["name"]);
-        this.createpart(x,y,obj["name"], this.parts);
+        this.createpart(x,y,obj["name"], this.parts, obj["stats"]);
         y += 50;
       }
     }
   },
 
-  createpart: function(x, y, sprite, grp) {
+  create: function() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    this.setupDisplay();
+    //Setup controls
+    var layerup = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+    layerup.onDown.add(this.moveSpriteUpLayer, this);
+    var cont = game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
+    cont.onDown.add(this.saveMonster, this);
+  },
+
+  getStat: function(key) {
+    used = this.getUsed()
+    sum = 0;
+    for (i in used) {
+      obj = used[i];
+      sum += obj.stats[key]
+    }
+    return sum;
+  },
+  
+  moveSpriteUpLayer: function() {
+    if (this.lastDragged) {
+      this.lastDragged.z = 0; 
+      this.parts.sort('z', Phaser.Group.SORT_ASCENDING);
+    }
+  },
+
+  saveMonster: function() {
+    console.log("Saving...");
+    var used = this.getUsed();
+    console.log(used.length + " sprites used.");
+  },
+
+  getUsed: function() {
+    used = [];
+    for (i in this.parts.children) {
+      obj = this.parts.children[i];
+      if (game.physics.arcade.overlap(obj, this.base, null, null, this))  {
+        used = used.concat(obj);
+      }
+    }   
+    return used;
+  },
+
+  assignLastDragged: function(sprite, pointer) {
+    this.lastDragged = sprite;
+  },
+
+  createpart: function(x, y, sprite, grp, stats) {
     p = grp.create(x,y,sprite);
     p.inputEnabled = true;
     p.input.enableDrag(true);
+    p.events.onDragStop.add(this.assignLastDragged, this);
+    game.physics.arcade.enable(p);
+    p.stats = stats;
   },
 
 
   update: function() {
-    game.debug.text("Monster values: ", 0, 50) 
+    game.debug.text("Monster values: ", 0, 50);
+    game.debug.text("Crazed: "+this.getStat("crazed"), 0, 70);
+    game.debug.text("Slimy: "+this.getStat("slimy"), 0, 80);
   },
 
 
